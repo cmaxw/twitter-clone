e2be7fbd79109f8486a8b8839696a9141416d147
Charles Max Wood <chuck@devchat.tv>
2014-02-27 19:56:38 -0700

    basic rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterClone::Application.config.secret_key_base = '829382119145b7db784f8a912a527fae79b16cdffc8520c9dc624548fa976846aaa17f1b3162502e7e96e9ff707f163d953ddee556146f85d400a910f845c1e4'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+TwitterClone::Application.routes.draw do
+  root to: "pages#index"
+end
